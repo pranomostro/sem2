3a37177980765442fb32e630183d1364fc1ec524
pranomostro <pranomostro@posteo.net>
2018-06-11 00:20:28 +0200

    Finished exercize 6.

--- ad_ü/7/RadixSort.java
+++ ad_ü/7/RadixSort.java
@@ -2,19 +2,37 @@ import java.lang.reflect.Array;
 import java.util.ArrayList;
 
 public class RadixSort {
-	private int key(Integer element, int digit) throws IllegalArgumentException {
-		// TODO
+	private static int key(Integer element, int digit) throws IllegalArgumentException {
+		if(Integer.toString(element).length()-1<digit)
+			return 0;
+		String nr=Integer.toString(element);
+		return nr.charAt(nr.length()-digit-1)-'0';
 	}
 
-	private void concatenate(ArrayList<Integer>[] buckets, Integer[] elements) {
-		// TODO
+	private static void kSort(Integer[] elements, int digit) {
+		ArrayList<ArrayList<Integer>> buckets=new ArrayList<ArrayList<Integer>>(10);
+
+		for(int i=0; i<10; i++)
+			buckets.add(new ArrayList<Integer>());
+
+		for(int i=0; i<elements.length; i++) {
+			buckets.get(key(elements[i], digit)).add(elements[i]);
+		}
+
+		int idx=0;
+		for(int i=0; i<buckets.size(); i++)
+			for(int j=0; j<buckets.get(i).size(); j++)
+				elements[idx++]=buckets.get(i).get(j);
 	}
 
-	private void kSort(Integer[] elements, int digit) {
-		// TODO
-	}
+	public static void sort(Integer[] elements) {
+		int d=0;
 
-	public void sort(Integer[] elements) {
-		// TODO
+		for(int i=0; i<elements.length; i++)
+			if(Math.floor(Math.log10(elements[i]))>d)
+				d=(int)Math.floor(Math.log10(elements[i]));
+
+		for(int i=0; i<=d; i++)
+			kSort(elements, i);
 	}
 }
--- ad_ü/7/RadixchenTest.java
+++ ad_ü/7/RadixchenTest.java
@@ -3,12 +3,17 @@ import java.util.Scanner;
 
 public class RadixchenTest {
 	public static void main(String[] args) {
-		Scanner scanner = new Scanner(System.in);
+		//Scanner scanner = new Scanner(System.in);
 
-		while (scanner.hasNextInt()) {
+		//while (scanner.hasNextInt()) {
 			// TODO: Eingabe in einer Liste speichern.
-		}
+		//}
 
 		// TODO: eingegebene Liste per RadixSort sortieren und zeilenweise ausgeben.
+		Integer[] testarr={41, 73, 98, 36, 93, 48, 39, 226, 10054, 19, 5, 22, 85, 37, 33, 34, 52, 82, 74, 44, 70, 55};
+		RadixSort.sort(testarr);
+
+		for(int i=0; i<testarr.length; i++)
+			System.out.println(testarr[i]);
 	}
 }
