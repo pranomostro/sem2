cd0b8171dae72436353930c40b8e9659f1bcc608
pranomostro <pranomostro@posteo.net>
2018-07-09 23:02:38 +0200

    Stuff.

---
+++ ad_ü/12/Dijkstra.java
@@ -0,0 +1,24 @@
+import java.util.List;
+
+public class Dijkstra {
+	public Dijkstra() {
+		// TODO
+		throw new RuntimeException("Not yet implemented");
+	}
+
+	public void findRoute(Graph g, Graph.Node a, Graph.Node b) {
+		// TODO
+		throw new RuntimeException("Not yet implemented");
+
+	}
+
+	public List<Graph.Node> getShortestPath() {
+		// TODO
+		throw new RuntimeException("Not yet implemented");
+	}
+
+	public Integer getShortestPathLength() {
+		// TODO
+		throw new RuntimeException("Not yet implemented");
+	}
+}
---
+++ ad_ü/12/Graph.java
@@ -0,0 +1,34 @@
+
+public class Graph {
+	public class Node {
+		// TODO
+
+		public Integer getID() {
+			// TODO
+			throw new RuntimeException("Not yet implemented");
+		}
+	}
+
+	public Graph() {
+		// TODO
+	}
+
+	public Integer addNode() {
+		// TODO
+		throw new RuntimeException("Not yet implemented");
+	}
+
+	public Graph.Node getNode(Integer id) {
+		// TODO
+		throw new RuntimeException("Not yet implemented");
+	}
+
+	/**
+	 * Fügt eine neue direktionale Kante von Knoten a zu Knoten b
+	 * mit Gewicht weight hinzu. Gewicht darf nicht negativ sein.
+	 */
+	public void addEdge(Graph.Node a, Graph.Node b, Integer weight) {
+		// TODO
+		throw new RuntimeException("Not yet implemented");
+	}
+}
